2679ec340b366c14e1c02e449f490c7b8e63da3a
Artem Danilov <danilov.arrtem@gmail.com>
2022-02-22 23:19:43 +0300

    feat: add cats table

---
+++ frontend/src/modules/cats/components/CatsTable.js
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from "react";
+import {Table} from "antd";
+import './CatsTable.css'
+import {getCats} from "../actions";
+import {COLUMNS} from "../config";
+
+const dataSource = [
+    {
+        key: '1',
+        name: 'Mike',
+        age: '10 лет',
+        weight: '32кг',
+        appearance_date: '2022-01-12T18:00:42.305Z',
+        breed:'синий ушастик'
+    },
+];
+
+export const CatsTable = () =>{
+    const [data, setData] = useState(undefined);
+
+    useEffect(()=>{
+        (async ()=>{
+            try{
+                const cats = await getCats();
+                setData(cats);
+            }catch (e) {
+                console.error(e, 'наташа, мы все уронили')
+            }
+
+        })()
+    },[])
+
+    return (
+        <>
+            <header className={'.CatsTable_header'}>
+                <h1>Комментарии</h1>
+            </header>
+
+            <Table
+                rowKey='id'
+                columns={COLUMNS}
+                dataSource={dataSource}
+                bordered
+            />
+
+        </>
+    );
+}
---
+++ frontend/src/modules/cats/config.js
@@ -0,0 +1,40 @@
+import {WatchButton} from "./components/WatchButton";
+import moment from "moment";
+
+export const COLUMNS = [
+    {
+        title: 'Кличка',
+        dataIndex: 'name',
+        key: 'title',
+    },
+    {
+        title: 'Возраст',
+        dataIndex: 'age',
+        align: 'center',
+    },
+    {
+        title: 'Вес',
+        dataIndex: 'weight',
+        align: 'center',
+    },
+    {
+        title: 'Дата появления в приюте',
+        dataIndex: 'appearance_date',
+        align: 'center',
+        render:(record) => <h1>{moment(record.appearance_date).format("DD/MM/YYYY")}</h1>,
+    },
+    {
+        title: 'Порода',
+        dataIndex: 'breed',
+        align: 'center',
+
+    },
+    {
+        title: 'Действие',
+        key: 'actions',
+        align: 'center',
+        render: (record) => (
+                <WatchButton />
+        ),
+    },
+];
