8014ec516fa71da02795261b0060bc43b5c24e73
Anton Pushpyshev <an.pushpyshev@gmail.com>
2022-02-22 23:38:04 +0300

    add wowners dao

---
+++ backend/src/main/java/ru/bnn/ArangoCatProject/DataAccessObject/OwnersDAO.java
@@ -0,0 +1,49 @@
+package ru.bnn.ArangoCatProject.DataAccessObject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import ru.bnn.ArangoCatProject.Model.Owners;
+import ru.bnn.ArangoCatProject.Repository.OwnersRepository;
+
+import java.util.Optional;
+
+@Service
+public class OwnersDAO {
+
+    private OwnersRepository ownersRepository;
+
+    @Autowired
+    public OwnersDAO(OwnersRepository ownersRepository) {
+        this.ownersRepository = ownersRepository;
+    }
+
+    public Owners save(Owners owners) {
+        return ownersRepository.save(owners);
+    }
+
+    public Optional<Owners> findOne(String ownerID) {
+        if (ownerID == null || ownerID.isEmpty()) return Optional.empty();
+        return ownersRepository.findById(ownerID);
+    }
+
+    public Iterable<Owners> findAll() {
+        return ownersRepository.findAll();
+    }
+
+    public Owners update(Owners owner) {
+        String ownerID = owner.get_id();
+        if (ownerID == null || ownerID.isEmpty())
+            return null;
+
+        return ownersRepository.save(owner);
+    }
+
+    public void delete(String ownerID) {
+        if (ownerID == null || ownerID.isEmpty())
+            return;
+        ownersRepository.deleteById(ownerID);
+    }
+}
