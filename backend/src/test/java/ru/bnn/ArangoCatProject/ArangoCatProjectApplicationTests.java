a0ed02f4491d0d75499f9654961a06d578b9dcb4
Anton Pushpyshev <an.pushpyshev@gmail.com>
2022-02-22 23:19:57 +0300

    back 0.1

---
+++ backend/src/test/java/ru/bnn/ArangoCatProject/ArangoCatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bnn.ArangoCatProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArangoCatProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
